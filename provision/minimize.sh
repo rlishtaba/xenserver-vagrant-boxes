5a4af34c3a458ab66e210d9c897938b78ef11ba3
Roman Lishtaba <roman@lishtaba.com>
2018-04-08 12:07:55 -0400

    adding minial Cent OS distro.

--- provision/minimize.sh
+++ provision/minimize.sh
@@ -8,13 +8,13 @@ esac
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
 count=$(($count-1))
 dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
-rm /tmp/whitespace
+rm -rf /tmp/whitespace
 
 # Whiteout /boot
 count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
 count=$(($count-1))
 dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
-rm /boot/whitespace
+rm -rf /boot/whitespace
 
 set +e
 swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`";
